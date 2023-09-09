19338f27c4539759d41bc20831f01ceadc0bf7cd
rewaa1 <142256963+rewaa1@users.noreply.github.com>
2023-09-09 03:36:02 +0300

    second problem

---
+++ datastructuresecondpr.js
@@ -0,0 +1,9 @@
+var set=[3, 1, 7, 9];
+var set2=[2, 4, 1, 9];
+ let sum = 0;
+ let nums = []
+ for (let i = 0; i < set.length; i++){
+     sum = set[i] + set2[i];
+     nums.push(sum)
+   }
+ console.log(nums)
