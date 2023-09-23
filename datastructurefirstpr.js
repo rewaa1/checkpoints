aaecb9d66b6c166d58d64a730ca38693b5abf50e
rewaa1 <142256963+rewaa1@users.noreply.github.com>
2023-09-23 19:42:43 +0300

    Update datastructurefirstpr.js

--- datastructurefirstpr.js
+++ datastructurefirstpr.js
@@ -1,8 +1,12 @@
-var set=[3, 1, 7, 9];
-var set2=[2, 4, 1, 9, 3];
-var sum= 0;
-for (var i = 0;i<= set2.length;i++){
-  sum += set2[i];
- 
-}
- console.log(sum)
+var arr1 = [3, 1, 7, 9];
+var arr2 = [2, 4, 1, 9, 3];
+var total=0;
+
+
+var unique1 = arr1.filter((o) => arr2.indexOf(o) === -1);
+var unique2 = arr2.filter((o) => arr1.indexOf(o) === -1);
+
+var unique = unique1.concat(unique2);
+for (var i = 0, sum = 0; i < unique.length; sum += unique[i++]);
+
+console.log(sum)
